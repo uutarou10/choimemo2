f7e8d8242581e24e1b79e0ff3ddb6c924dce1211
Kota Nonaka <kota.kaicho@gmail.com>
2018-10-12 16:16:47 +0900

    Add redux devtools extension.

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,7 @@
 import { connectRouter, routerMiddleware } from 'connected-react-router';
 import { createBrowserHistory } from 'history';
 import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './module';
 
@@ -8,8 +9,10 @@ export const history = createBrowserHistory();
 
 export default createStore(
   connectRouter(history)(rootReducer),
-  applyMiddleware(
-    thunk,
-    routerMiddleware(history)
+  composeWithDevTools(
+    applyMiddleware(
+      thunk,
+      routerMiddleware(history)
+    )
   )
 );
